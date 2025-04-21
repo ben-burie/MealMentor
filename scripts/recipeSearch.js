a8ac3b403346132723723903b6b13ac4d235c33b
ben-burie <benburie22@gmail.com>
2025-04-21 14:40:57 -0500

    added local storage functionality for recipe table

--- scripts/recipeSearch.js
+++ scripts/recipeSearch.js
@@ -1,14 +1,18 @@
 var groceries = JSON.parse(localStorage.getItem("groceries")) || [];
+var recipes = JSON.parse(localStorage.getItem("recipes")) || [];
 
 const recipeForm = document.getElementById("recipe-select");
 const recipeDropdown = document.getElementById("recipes-dropdown");
 const addRecipeButton = document.getElementById("submit-recipe");
 const groceryClearButton = document.getElementById("clear-groceries");
+const recipeClearButton = document.getElementById("clear-recipes");
 
 window.addEventListener("load", populateRecipeOptions);
 window.addEventListener("load", populateGroceries);
+window.addEventListener("load", populateRecipes);
 addRecipeButton.addEventListener("click", addRecipe);
 groceryClearButton.addEventListener("click", () => clear("groceries", "grocery-table"));
+recipeClearButton.addEventListener("click", () => clear("recipes", "recipe-table"));
 
 
 function populateRecipeOptions() {
@@ -56,10 +60,34 @@ function addRecipe(e) {
         const linkCell = document.createElement("td");
         linkCell.innerHTML = "<a href='https://" + foundRecipe.link + "' target='_blank'>Link</a>";
         newRow.appendChild(linkCell);
+        currentRecipe = [foundRecipe.name, foundRecipe.link];
+        recipes.push(currentRecipe);
+        localStorage.setItem("recipes", JSON.stringify(recipes));
         refreshGroceries(foundRecipe.ingredients, kitchen);
     }
 
     recipeDropdown.value = "Select a recipe";
+}
+
+function populateRecipes() {
+    const table = document.getElementById("recipe-table");
+    for (let i=0; i<recipes.length; i++) {
+        const newRow = document.createElement("tr");
+        const nameCell = document.createElement("td");
+        const linkCell = document.createElement("td");
+        nameCell.innerHTML = recipes[i][0];
+        linkCell.innerHTML = "<a href='https://" + recipes[i][1] + "' target='_blank'>Link</a>";
+        
+        newRow.appendChild(nameCell);
+        newRow.appendChild(linkCell);
+        table.appendChild(newRow);
+        newRow.addEventListener("click", () => {
+            if (confirm("Delete " + newCell.textContent + "?") == true) {
+                newRow.remove();
+                removeItemFromArray(newCell.textContent, recipes, "recipes");
+            }
+        });
+    } 
 }
 
 function refreshGroceries(ingredients, kitchen) {
@@ -129,6 +157,10 @@ function removeItemFromArray(requestedItem, list, title) { //removes requested i
 }
 
 function clear(title, tableTitle) { //clears table contents from local memory
+    if (localStorage.getItem(title) == null) {
+        alert("Your " + title + " list is already empty!");
+        return;
+    }
     if (confirm("Are you sure you want to clear your " + title + " list?") == true) {
         localStorage.removeItem(title);
         const table = document.getElementById(tableTitle);
