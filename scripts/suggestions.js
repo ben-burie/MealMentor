52eef6661b2cb08c6cc58d30ef27b7b9ab740654
ben-burie <benburie22@gmail.com>
2025-04-30 10:57:34 -0500

    Algorithm for finding valid recipes to suggest

--- scripts/suggestions.js
+++ scripts/suggestions.js
@@ -6,4 +6,32 @@ function refreshSuggestions() {
     if (kitchen.length == 0) {
         alert("No recipes available, there are not enough groceries in your kitchen.");
     }
+    else {
+        findAvailableRecipes();
+    }
+}
+
+function findAvailableRecipes() {
+    fetch("data/recipe_database.json")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(data => {
+            let ingredients = data.ingredients;
+            let length = ingredients.length;
+            let isValid = false;
+            for (i=0; i<length; i++) {
+                if (!kitchen.includes(ingredients[i])) {
+                    isValid = false;
+                    break;
+                }
+                else {
+                    isValid = true;
+                }
+            }
+            if (isValid == true) {
+                console.log(data.name); //currently logging all recipes that can be made with available ingredients
+            }
+        });
+    })
+    .catch(error => console.error('Error loading JSON:', error));
 }
